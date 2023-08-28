fa8987aad47945e87f02e6bcb5cd759077086fc4
deniandriancode <deniandriancode@gmail.com>
2023-08-28 23:51:08 +0700

    update

--- main.py
+++ main.py
@@ -9,25 +9,18 @@ def make_request(url):
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
     }
 
-    response = httpx.get(url, headers=headers, follow_redirects=True).text
+    response = httpx.get(url, headers=headers).text
 
     soup = BeautifulSoup(response, "html.parser")
 
-    try:
-        t = soup.find("div", class_="entry-content").find("style").get_text()
-        style_raw = t
-    except:
-        style_raw = soup.find("div", id="title-hero").find("style").get_text()
+    style_raw = soup.find("div", class_="entry-content").find("style").get_text()    
         
     style_raw = style_raw.split("background-image: ")[-1].replace("\n", "").replace("\t", "").replace("\");}}", "").strip()
 
     image_url = style_raw.split("url(\"")[1]
     common_name = soup.find("h1").get_text().strip()
 
-    try:
-        latin_name = soup.find("p", class_="has-text-align-center text-white font-weight-bolder font-size-lg").get_text().strip()
-    except:
-        lating_name = "Not provided"
+    latin_name = soup.find("p", class_="has-text-align-center text-white font-weight-bolder font-size-lg").get_text().strip()
     print(f"GET {common_name}")
 
     return {
